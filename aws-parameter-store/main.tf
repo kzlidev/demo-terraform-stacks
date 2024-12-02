dfa0a29910a9f5f04f96be6dd60a8ac31d897d51
kz.li <kz.li@hashicorp.com>
2024-12-02 16:20:23 +0800

    Fixed errors

--- aws-parameter-store/main.tf
+++ aws-parameter-store/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "foo" {
-  for_each = var.key_value_pairs
-  type     = "String"
-  name     = each.key
-  value    = each.value
+  count = length(var.key_value_pairs)
+  type  = "String"
+  name  = var.key_value_pairs[0].key
+  value = var.key_value_pairs[0].value
 }
--- variables.tfstack.hcl
+++ variables.tfstack.hcl
@@ -19,3 +19,11 @@ variable "default_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type = string
+}
